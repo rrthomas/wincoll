4957ec5827a8b370f26424af86e9922b3296faeb
Reuben Thomas <rrt@sc3d.org>
2024-12-11 14:34:12 +0000

    GitHub Actions: Factor out dependency installation

---
+++ .github/install-dependencies.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Ubuntu
+if [ "$RUNNER_OS" = "Linux" ]; then
+    sudo apt-get -y install python3-build python3-venv python3-pygame python3-importlib-resources make
+    pip install --user pyscroll
+fi
+
+# macOS
+if [ "$RUNNER_OS" = "macOS" ]; then
+    brew install tox
+    # Prepend optional brew binary directories to PATH
+    echo "$(brew --prefix)/opt/python/libexec/bin" >> $GITHUB_PATH
+    pip install --user --break-system-packages pyscroll
+    # Prepend user bin directory to PATH
+    echo $(python -c "import os; import sysconfig; print(sysconfig.get_path('scripts', f'{os.name}_user'))") >> $GITHUB_PATH
+fi
