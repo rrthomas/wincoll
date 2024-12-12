5117c0ce5dc656a293b93048e799a1375e848c86
Reuben Thomas <rrt@sc3d.org>
2024-12-12 21:40:22 +0000

    Install all Python dependencies on macOS

--- .github/install-dependencies.sh
+++ .github/install-dependencies.sh
@@ -11,7 +11,7 @@ if [ "$RUNNER_OS" = "macOS" ]; then
     brew install tox
     # Prepend optional brew binary directories to PATH
     echo "$(brew --prefix)/opt/python/libexec/bin" >> $GITHUB_PATH
-    pip install --user --break-system-packages pyscroll
+    pip install --user --break-system-packages build pygame importlib-resources typing-extensions platformdirs pyscroll pytmx
     # Prepend user bin directory to PATH
     echo $(python -c "import os; import sysconfig; print(sysconfig.get_path('scripts', f'{os.name}_user'))") >> $GITHUB_PATH
 fi
